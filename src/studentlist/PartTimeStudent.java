d75e0ce21933fbc2b7416098ff1ad75356224d12
megha <megha.patel2@sheridancollege.ca>
2023-02-02 11:29:45 -0500

    parttimestudent class added

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package studentlist;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class PartTimeStudent {
+    
+    private int numOfHours;
+
+    public PartTimeStudent(int numOfHours) {
+        this.numOfHours = numOfHours;
+    }
+
+    public int getNumOfHours() {
+        return numOfHours;
+    }
+
+    public void setNumOfHours(int numOfHours) {
+        this.numOfHours = numOfHours;
+    }
+
+}
